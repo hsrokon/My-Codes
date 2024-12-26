4c6cb88090d8bdc1738d0b65b9c308cd46831579
MD. HUSSAIN SHAHRIAR ROKON <hsrokonofficial@gmail.com>
2024-12-26 15:31:07 +0600

    Counted and analyzed user data and rendered it on page

--- scripts/fetching.js
+++ scripts/fetching.js
@@ -22,24 +22,52 @@
 // fetchTest();
 
 //Filtering data
-async function filterFetchTest () {
-    try {
-        const response =  await fetch('https://jsonplaceholder.typicode.com/posts');
-        const posts= await response.json();
-        const filteredPosts = posts.filter(post => post.userId ===2);
+// async function filterFetchTest () {
+//     try {
+//         const response =  await fetch('https://jsonplaceholder.typicode.com/posts');
+//         const posts= await response.json();
+//         const filteredPosts = posts.filter(post => post.userId ===2);
         
-        console.log(filteredPosts);
-        const postContainer = document.getElementById('post-container');
+//         console.log(filteredPosts);
+//         const postContainer = document.getElementById('post-container');
 
-        filteredPosts.forEach(somePosts => {
-            const createdDiv = document.createElement('div');
-            createdDiv.innerHTML=`
-            <h1>${somePosts.title}</h1>
+//         filteredPosts.forEach(somePosts => {
+//             const createdDiv = document.createElement('div');
+//             createdDiv.innerHTML=`
+//             <h1>${somePosts.title}</h1>
+//             `
+//             postContainer.appendChild(createdDiv);
+//         })
+//     } catch (error) {
+//         console.error(error);
+//     }
+// }
+// filterFetchTest();
+
+//Count and analyze user data
+async function analyzeUserData() {  
+    try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+        const posts = await response.json();
+        
+        const userPosts = {};
+        const userCountedPost = document.getElementById('post-container');
+        posts.forEach(post => {
+            userPosts[post.userId] = (userPosts[post.userId] || 0) +1; //checks if the user has any post in our object(if no then by default 0) then adds 1 after each loop count then saves it on our created object in format like:(api's user id : api's post count) and This counts the total number of posts for each userId and stores it in the userPosts object.
+        });
+        
+        Object.keys(userPosts).forEach(userId => { //Object.keys(userPosts) recovers an array of all user IDs.
+            const postElement = document.createElement('div');
+            postElement.innerHTML=`
+            <div class="flex justify-evenly">
+            <h2>User Id: ${userId}</h2>
+            <p>Total post: ${userPosts[userId]}</p>
+            </div>
             `
-            postContainer.appendChild(createdDiv);
+            userCountedPost.appendChild(postElement);
         })
     } catch (error) {
         console.error(error);
     }
 }
-filterFetchTest();
+analyzeUserData();
